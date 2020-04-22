0dc2853721d57c21dd8d1e75d8f083c2d3225841
0deepsleep <0deepsleep@gmail.com>
2020-04-22 20:22:52 +0900

    앱 아이콘 바꾸고 이름 지정한(초기작업)

---
+++ app/src/main/java/dasdsa/sdn/pizzastoreapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package dasdsa.sdn.pizzastoreapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun setupEvents() {
+
+    }
+
+    override fun setValue() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
